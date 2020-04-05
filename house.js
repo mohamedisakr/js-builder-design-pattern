de043f2a28a104d96e1a1b91264c8e89030cd87e
mohamedisakr <mohamedisakr@gmail.com>
2020-04-05 10:15:50 +0200

    implementing the whole functionality

--- house.js
+++ house.js
@@ -1,7 +1,17 @@
 export default class House {
-  constructor(windows, doors, rooms) {
+  constructor(
+    windows,
+    doors,
+    rooms,
+    { backyard, garage, heating, swimpool, garden } = {}
+  ) {
     this.windows = windows;
     this.doors = doors;
     this.rooms = rooms;
+    this.backyard = backyard;
+    this.garage = garage;
+    this.heating = heating;
+    this.swimpool = swimpool;
+    this.garden = garden;
   }
 }
--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 // https://refactoring.guru/design-patterns/builder
+import House from "./house.js";
+import Backyard from "./backyard.js";
+import Garage from "./garage.js";
+import Heating from "./heating.js";
+import Swimpool from "./swimpool.js";
+import Garden from "./garden.js";
+
+// house options
+const backyard = new Backyard(1000);
+const garage = new Garage(2000);
+const heating = new Heating("full", 380);
+const swimpool = new Swimpool(2200);
+const garden = new Garden(3400);
+
+// house
+const house = new House(21, 3, 100, {
+  backyard,
+  garage,
+  heating,
+  swimpool,
+  garden
+});
+
+console.log(house);
